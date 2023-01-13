910483729ce9c20c3fbfc3d0a3d77e1fa0df81fe
zhangkai-dot <1216318121@qq.com>
2023-01-13 17:49:20 +0800

    第一次提交

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+// .vuepress/config.js
+module.exports = {
+  themeConfig: {
+    logo: "/logo.jpg",
+    nav: [
+      { text: "首页", link: "/" },
+      { text: "前端", link: "/Frontend/" },
+      { text: "工具", link: "/Tools/" },
+    ],
+    // sidebar: ["/Frontend/", "/Tools/"],
+  },
+};
---
+++ docs/Tools/README.md
@@ -0,0 +1,8 @@
+---
+title: 工具
+sidebar: auto
+---
+
+::: tip
+推荐自己再用油猴插件工具：[点我跳转](https://wwpm.lanzouf.com/iQ8HR0kwwjze) 密码:1546
+:::
